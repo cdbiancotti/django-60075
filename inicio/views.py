9210e6de292e13a7c34ae4a227930bfe1a216008
Cristian D Biancotti <cdbiancotti@gmail.com>
2024-10-01 21:01:30 -0300

    Clase Portfolio II, sin modelos

---
+++ inicio/views.py
@@ -0,0 +1,55 @@
+from django.http import HttpResponse
+from django.template import Template, Context, loader
+from datetime import datetime
+from django.shortcuts import render
+
+def mi_vista(request):
+    return HttpResponse('Hola soy la vista')
+
+def inicio(request):
+    return HttpResponse('<h1>Soy la pantalla de inicio </h1>')
+
+def vista_datos1(request, nombre):
+    nombre_mayuscula = nombre.upper()
+    return HttpResponse(f'Hola {nombre_mayuscula}!!')
+
+def primer_template(request):
+    
+    # sin with
+    # archivo_del_template = open(r'templates\primer_template.html')
+    # template = Template(archivo_del_template.read())
+    # archivo_del_template.close()
+    
+    # con with
+    with open(r'templates\primer_template.html') as archivo_del_template:
+        template = Template(archivo_del_template.read())
+    
+    contexto = Context()
+    
+    render_template = template.render(contexto)
+    
+    return HttpResponse(render_template)
+
+
+def segundo_template(request):
+    
+    fecha_actual = datetime.now()
+    datos = {
+        'fecha_actual': fecha_actual,
+        'numeros': list(range(1, 11))    
+    }
+    
+    # v1
+    # with open(r'templates\segundo_template.html') as archivo_del_template:
+    #     template = Template(archivo_del_template.read())
+    # contexto = Context(datos)
+    # render_template = template.render(contexto)
+    # return HttpResponse(render_template)
+
+    # v2
+    # template = loader.get_template('segundo_template.html')
+    # render_template = template.render(datos)
+    # return HttpResponse(render_template)
+
+    # v3
+    return render(request, 'segundo_template.html', datos)
