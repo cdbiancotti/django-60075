c98ef661d590ab65f1d371bdfed82d363f952c3e
Cristian D Biancotti <cdbiancotti@gmail.com>
2024-10-08 20:46:19 -0300

    Clase 20 - Playground Intermendio II

--- inicio/models.py
+++ inicio/models.py
@@ -5,3 +5,6 @@ class Auto(models.Model):
     marca = models.CharField(max_length=20)
     modelo = models.CharField(max_length=20)
     anio = models.IntegerField()
+    
+    def __str__(self):
+        return f'{self.id} {self.modelo} {self.anio}'
